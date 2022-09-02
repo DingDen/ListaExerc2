6418fd7725eb8a1029b340dd329b83d60de9cd88
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-02 18:11:36 -0300

    Update Quest3.py

--- Quest3.py
+++ Quest3.py
@@ -1,2 +1,20 @@
 '''3. Faça um Programa que leia um número e exiba o dia correspondente da semana. 
 (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.'''
+
+DIA = int(input('Digite um número inteiro no intervalo de 0 a 6 para escolher um dia da semana:\n'))
+if DIA == 0:
+	print('\tDOMINGO!')
+elif DIA == 1:
+	print('\tSEGUNDA!')
+elif DIA == 2:
+	print('\tTERÇA!')
+elif DIA == 3:
+	print('\tQUARTA!')
+elif DIA == 4:
+	print('\tQUINTA!')
+elif DIA == 5:
+	print('\tSEXTA!')
+elif DIA == 6:
+	print('\tSÁBADO!')
+else:
+	print('\tVALOR INVÁLIDO!')
