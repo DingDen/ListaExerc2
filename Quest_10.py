9e5e19b3c380908417f1f43249ff5fcecea79b3c
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 01:48:26 -0300

    Update Quest_10.py

--- Quest_10.py
+++ Quest_10.py
@@ -1,2 +1,5 @@
-'''10. Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:'''
-
+'''10. Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:
+e. A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada; 
+f. A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada; 
+g. A mensagem "Aprovado com Distinção", se a média for igual a 10.
+'''
