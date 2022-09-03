8b5e9f479e80424c66606f1d9304767ac6a58b8a
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 14:50:50 -0300

    Update Quest_10.py

--- Quest_10.py
+++ Quest_10.py
@@ -3,3 +3,15 @@ e. A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva m
 f. A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada; 
 g. A mensagem "Aprovado com Distinção", se a média for igual a 10.
 '''
+
+NOTA1 = float(input('Digite sua nota da 1AV: '))
+NOTA2 = float(input('Digite sua nota da 2AV: '))
+NOTA3 = float(input('Digite sua nota da 3AV: '))
+MEDIA = (NOTA1 + NOTA2 + NOTA3) / 3
+
+if (MEDIA == 10):
+	print(f'\n\tAPROVADO COM DISTINÇÃO ------ Média: {MEDIA:.1f}')			
+elif (MEDIA < 7):
+	print(f'\n\tREPROVADO ------ Média: {MEDIA:.1f}')
+else:
+	print(f'\n\tAPROVADO ------ Média: {MEDIA:.1f}')
