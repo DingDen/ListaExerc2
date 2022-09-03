7b00e88e59718a8988720c7e0aa80cadbae20089
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 17:22:42 -0300

    Update Quest_13.py

--- Quest_13.py
+++ Quest_13.py
@@ -1,1 +1,8 @@
 '''13. Faça um Programa que peça um número e informe se o número é inteiro ou decimal.'''
+
+NUM = float(input('\tDigite um número real: '))
+
+if (NUM % 1 == 0):
+	print('\tO número é inteiro!')
+else:
+	print('\tO número é decimal!')
