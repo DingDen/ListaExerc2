268063d42518e56af90e34bff9bc47fdda0ebd6d
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 01:52:06 -0300

    Create Quest_15.py

---
+++ Quest_15.py
@@ -0,0 +1,9 @@
+'''15. Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são: 
+m. "Telefonou para a vítima?" 
+n. "Esteve no local do crime?" 
+o. "Mora perto da vítima?" 
+p. "Devia para a vítima?" 
+q. "Já trabalhou com a vítima?" O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
+Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino".
+Caso contrário, ele será classificado como "Inocente". 
+'''
