170677a003e7e005d0aa24732d4449a47523a414
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 16:58:11 -0300

    Update Quest_12.py

--- Quest_12.py
+++ Quest_12.py
@@ -1,1 +1,8 @@
 '''12. Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar.'''
+
+NUM = int(input('\tDigite um número inteiro: '))
+
+if (NUM % 2 == 0):
+	print(f'\t{NUM} é um número par!')
+else:
+	print(f'\t{NUM} é um número ímpar!')
