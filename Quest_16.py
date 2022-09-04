b2292525b78c3d9c5e4b368eef17b50917bec8c1
DingDen <87573646+DingDen@users.noreply.github.com>
2022-09-03 22:34:23 -0300

    Update Quest_16.py

--- Quest_16.py
+++ Quest_16.py
@@ -8,3 +8,26 @@ w. acima de 20 litros, desconto de 6% por litro
 Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: 
 A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90. 
 '''
+
+COMB = str(input('Qual o tipo de combustível? [A] para álcool ou [G] para gasolina: '))
+LITROS = int(input('Quantos litros de combustível? '))
+if (COMB == 'A'):
+	NOME = 'Álcool'
+	PRECO = 1.9
+	if (LITROS <= 20):
+		DESCONTO = LITROS*PRECO*0.03
+		TOTAL =  (PRECO*LITROS) - DESCONTO
+	else:
+		DESCONTO = LITROS*PRECO*0.05
+		TOTAL = (PRECO*LITROS) - DESCONTO	
+else:
+	NOME = 'Gasolina'
+	PRECO = 2.5
+	if (LITROS <= 20):
+		DESCONTO = LITROS*PRECO*0.04
+		TOTAL = 	(PRECO*LITROS) - DESCONTO
+	else:
+		DESCONTO = LITROS*PRECO*0.06
+		TOTAL = (PRECO*LITROS) - DESCONTO
+
+print(f'\n\t{LITROS} litro(s) vendidos\n\t{COMB}-{NOME}\n\tR${TOTAL:.2f}')
